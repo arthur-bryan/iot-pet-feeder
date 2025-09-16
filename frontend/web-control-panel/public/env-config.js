ea0eda8d1c608c9aa4fe6035158c5b987dcd7da6
arthur-bryan <arthurbryan2030@gmail.com>
2025-09-16 08:29:07 -0300

    Fixes in web authentication

--- frontend/web-control-panel/public/env-config.js
+++ frontend/web-control-panel/public/env-config.js
@@ -1,4 +1,3 @@
 window.ENV = {
-  VITE_API_BASE_URL: "https://hkwon40nol.execute-api.us-east-2.amazonaws.com/dev" // Change to your actual backend URL
+  VITE_API_BASE_URL: "https://hkwon40nol.execute-api.us-east-2.amazonaws.com/dev/api/v1" // Change to your actual backend URL
 };
-
